f99206339a43dffbc46bdc35ccb5b7bdf9b520fe
BragaJoao <joaogbraga@hotmail.com.br>
2022-07-07 18:50:10 -0300

    chore: findById, update, delete and middlewares

--- src/characters/characters.controllers.js
+++ src/characters/characters.controllers.js
@@ -10,6 +10,15 @@ const readAllCharactersUrlController = async (req, res) => {
   res.send(characters);
 };
 
+const readCharacterByIdUrlController = async (req, res) => {
+  const id = req.params.id;
+  const chosenCharacter = await characterService.readCharacterByIdUrlService(id);
+  if (!chosenCharacter) {
+    return res.status(404).send({ message: 'Character not found!!' });
+  }
+  res.status(200).send(chosenCharacter);
+};
+
 const createCharacterUrlController = async (req, res) => {
   const character = req.body;
   const newCharacter = await characterService.createCharacterUrlService(
@@ -18,7 +27,25 @@ const createCharacterUrlController = async (req, res) => {
   res.status(201).send(newCharacter);
 };
 
+const updateCharacterUrlController = async (req, res) => {
+    const id= req.params.id;
+
+    const editedCharacter = req.body;
+    const updatedCharacter = await characterService.updateCharacterUrlService(id, editedCharacter);
+
+    res.status(200).send(updatedCharacter);
+}
+
+const deleteCharacterUrlController = async (req, res) => {
+  const id = req.params.id; 
+  await characterService.deleteCharacterUrlService(id);
+  res.status(200).send({ message: "Character was successfully deleted" });
+};
+
 module.exports = {
   readAllCharactersUrlController,
+  readCharacterByIdUrlController,
   createCharacterUrlController,
+  updateCharacterUrlController,
+  deleteCharacterUrlController,
 };
--- src/characters/characters.services.js
+++ src/characters/characters.services.js
@@ -5,12 +5,30 @@ const readAllCharactersUrlService = async () => {
   return characters;
 };
 
+const readCharacterByIdUrlService = async (id) => {
+  const character = await Character.findById(id);
+  return character;
+};
+
 const createCharacterUrlService = async (character) => {
   const newCharacter = await Character.create(character);
   return newCharacter;
 };
 
+const updateCharacterUrlService = async (id, editedCharacter) => {
+  const updatedCharacter = await Character.findByIdAndUpdate(id,editedCharacter).setOptions({ returnOriginal: false })
+
+  return updatedCharacter
+};
+
+const deleteCharacterUrlService = async (id) => {
+  return await Character.findByIdAndDelete(id);
+};
+
 module.exports = {
   readAllCharactersUrlService,
+  readCharacterByIdUrlService,
   createCharacterUrlService,
+  updateCharacterUrlService,
+  deleteCharacterUrlService,
 };
